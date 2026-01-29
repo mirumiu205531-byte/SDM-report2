4a70431bd0b90f67e591e500ce2a2d2e7de6cfac
Yashiki Miu <mirumiu205531@gmail.com>
2026-01-29 16:18:30 +0900

    Fix calc function and add tests

--- calc_mul.py
+++ calc_mul.py
@@ -1,34 +1,35 @@
 #!/usr/bin/python3
-
 import re
-                
-def calc(A,B):
-        ai=str(A)
-        bi=str(B)
-        p = re.compile('\d+(\.\d+)?')
-        if p.match(ai) or p.match(bi):
-                a=float(ai)
-                b=float(bi)
-                if 0<a and a<b and b<1000:
-                        valid=True
-                else:
-                        valid=False
-        else:
-                valid=False
-                
-        if valid:
-                ans=a*b
-                return ans
-        else:
-                return -1
+
+def calc(A, B):
+    if isinstance(A, str):
+        if not re.fullmatch(r'\d+', A):
+            return -1
+        A = int(A)
         
-                
-def main ():
-	matchstring = ''
-	while matchstring != 'end':
-                A = input ('input A: ')
-                B = input ('input B: ')
-                print ('input A * input B = ', calc(A,B))
+    if isinstance(B, str):
+        if not re.fullmatch(r'\d+', B):
+            return -1
+        B = int(B)
+
+    if isinstance(A, int) and isinstance(B, int):
+        if 1 <= A <= 999 and 1 <= B <= 999:
+            return A * B
+            
+    return -1
+
+def main():
+    while True:
+        try:
+            line = input('input A: ')
+            if line == 'end': break
+            A = line
+            line = input('input B: ')
+            if line == 'end': break
+            B = line
+            print('input A * input B = ', calc(A, B))
+        except EOFError:
+            break
 
 if __name__ == '__main__':
-	main()
+    main()
